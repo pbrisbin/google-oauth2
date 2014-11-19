2eec515fd2406930bed12142dd65d0968242e9c7
patrick brisbin <pbrisbin@gmail.com>
2014-11-18 22:06:13 -0500

    Address hlint suggestions

--- test/Network/Google/OAuth2Spec.hs
+++ test/Network/Google/OAuth2Spec.hs
@@ -17,37 +17,34 @@ main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec = do
-    describe "Token exchange" $ do
-        it "prompts the user and returns an access token" $ do
-            pendingWith "Requires interaction. Do not run regularly"
+spec = describe "Token exchange" $ do
+    it "prompts the user and returns an access token" $ do
+        pendingWith "Requires interaction. Do not run regularly"
 
-            loadEnv
+        token <- getToken Nothing
 
-            client <- OAuth2Client
-                <$> getEnv "CLIENT_ID"
-                <*> getEnv "CLIENT_SECRET"
+        token `shouldSatisfy` (not . null)
 
-            scope <- getEnv "EXAMPLE_SCOPE"
-            token <- getAccessToken client [scope] Nothing
+    -- N.B. requires interaction on first run only
+    it "grants access to an API" $ do
+        token <- getToken $ Just "test/oauth.token"
 
-            token `shouldSatisfy` (not . null)
+        request <- parseUrl =<< getEnv "EXAMPLE_URL"
+        response <- withManager $ httpLbs $ authorize token request
 
-        -- N.B. requires interaction on first run only
-        it "grants access to an API" $ do
-            loadEnv
+        responseBody response `shouldSatisfy` (not . L8.null)
 
-            client <- OAuth2Client
-                <$> getEnv "CLIENT_ID"
-                <*> getEnv "CLIENT_SECRET"
+getToken :: Maybe FilePath -> IO OAuth2Token
+getToken mfp = do
+    loadEnv
 
-            scope <- getEnv "EXAMPLE_SCOPE"
-            token <- getAccessToken client [scope] (Just "test/oauth.token")
+    client <- OAuth2Client
+        <$> getEnv "CLIENT_ID"
+        <*> getEnv "CLIENT_SECRET"
 
-            request <- parseUrl =<< getEnv "EXAMPLE_URL"
-            response <- withManager $ httpLbs $ authorize token request
+    scope <- getEnv "EXAMPLE_SCOPE"
 
-            responseBody response `shouldSatisfy` (not . L8.null)
+    getAccessToken client [scope] mfp
 
 authorize :: OAuth2Token -> Request -> Request
 authorize token request = request
